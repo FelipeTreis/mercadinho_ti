f965c77fd5c0029f716f8dd8fc8d0154768f0737
Felipe Treis de Souza <felipetreis1@gmail.com>
2024-06-01 13:25:35 -0300

    Criado os modelos MovimentoEstoque e Venda

--- app/models.py
+++ app/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+OPERACAO = (
+    ('entrada', 'entrada'),
+    ('saida', 'saida'),
+)
+
+ORIGEM = (
+    ('compra', 'compra'),
+    ('venda', 'venda'),
+    ('estorno_carrinho', 'estorno_carrinho'),
+)
+
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=60, unique=True)
@@ -34,6 +45,18 @@ class Estoque(models.Model):
         return f'{self.produto.nome} - {self.quantidade} unidade(s)'
     
 
+class MovimentoEstoque(models.Model):
+    produto = models.ForeignKey(Produto, related_name='movimentos_estoques', on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField(null=False, blank=False)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    operacao = models.CharField(choices=OPERACAO, max_length=10, null=False, blank=False)
+    origem = models.CharField(choices=ORIGEM, max_length=20, null=False, blank=False)
+    movimentado = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.operacao} - {self.produto.nome} - {self.quantidade} unidade(s)'
+    
+
 class Carrinho(models.Model):
     produtos = models.ManyToManyField(Produto, through='CarrinhoProduto')    
 
@@ -47,3 +70,12 @@ class CarrinhoProduto(models.Model):
 
     def __str__(self):
         return f'{self.quantidade} x {self.produto.nome}'
+    
+
+class Venda(models.Model):
+    carrinho = models.ForeignKey(CarrinhoProduto, on_delete=models.CASCADE)    
+    matricula_colaborador = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.matricula_colaborador} - {self.carrinho.produto} - {self.carrinho.quantidade} un. - R${self.carrinho.valor}'
