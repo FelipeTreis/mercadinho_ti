9d01be1f7ec2d3ad2dc73a25dca40ee452c314a4
Felipe Treis de Souza <felipetreis1@gmail.com>
2024-06-03 18:31:45 -0300

    Adicionado menu lateral

---
+++ app/base_statics/global/js/script.js
@@ -0,0 +1,29 @@
+(() => {
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
+  
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+  
+    const closeMenu = () => {
+      buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+      menuContainer.classList.add(menuHiddenClass);
+    };
+  
+    const showMenu = () => {
+      buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+      menuContainer.classList.remove(menuHiddenClass);
+    };
+  
+    if (buttonCloseMenu) {
+      buttonCloseMenu.removeEventListener('click', closeMenu);
+      buttonCloseMenu.addEventListener('click', closeMenu);
+    }
+  
+    if (buttonShowMenu) {
+      buttonCloseMenu.removeEventListener('click', showMenu);
+      buttonShowMenu.addEventListener('click', showMenu);
+    }
+  }
+)();
